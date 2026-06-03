6075cfbbcb7deaf4a36890cdb0cb99d57ab63500
Angel1485 <andyjp_666@hotmail.com>
2026-06-02 22:42:54 -0500

    Colaborador Andres

--- practica_conflictos/conflictos.js
+++ practica_conflictos/conflictos.js
@@ -16,6 +16,12 @@ const integrantes = [
         nombre: "Desarrollador Base",
         rol: "Fullstack Developer",
         bio: "Estructura inicial del sistema lista para recibir las tarjetas de los nuevos ingenieros del equipo."
+    },
+    {
+        id: 3,
+        nombre: "Andres Sarango Colaborador",
+        rol: "Fronted Enginner",
+        bio: "Especialista en Sistemas Integrados."
     }
 ];
 
